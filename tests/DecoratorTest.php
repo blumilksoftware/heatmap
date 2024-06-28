361da3e7fc0ee3b8ade748d88b33b0222b1f4a34
BarTracz <bartosztracz68@gmail.com>
2024-06-28 11:51:38 +0200

    test fix

--- tests/DecoratorTest.php
+++ tests/DecoratorTest.php
@@ -29,7 +29,6 @@ class DecoratorTest extends TestCase
             ["created_at" => "2022-11-16 00:00:00"],
             ["created_at" => "2022-11-16 00:00:00"],
             ["created_at" => "2022-11-18 00:00:00"],
-            ["created_at" => "2022-11-19 00:00:00"],
         ];
 
         $builder = new HeatmapBuilder(
@@ -41,7 +40,7 @@ class DecoratorTest extends TestCase
         $result = $builder->build($data);
 
         $this->assertSame(
-            expected: [0, 2, 5, 0, 7, 0, 1, 1],
+            expected: [0, 0, 2, 5, 0, 7, 0, 1],
             actual: array_map(fn(Tile $item): int => $item->count, $result),
         );
 
@@ -50,18 +49,47 @@ class DecoratorTest extends TestCase
         $this->assertSame(
             expected: array_map(
                 fn(string $class): string => "$defaultClasses $class",
-                ["bg-white", "bg-green-100", "bg-green-600", "bg-white", "bg-green-900", "bg-white", "bg-green-50", "bg-green-50"],
+                ["bg-white", "bg-white", "bg-green-100", "bg-green-600", "bg-white", "bg-green-900", "bg-white", "bg-green-50"],
             ),
             actual: array_map(fn(Tile $item): string => $item->description, $result),
         );
+    }
+
+    public function testTodayAndFuture(): void
+    {
+        $data = [
+            ["created_at" => "2022-11-13 00:00:00"],
+            ["created_at" => "2022-11-13 00:00:00"],
+            ["created_at" => "2022-11-14 00:00:00"],
+            ["created_at" => "2022-11-14 00:00:00"],
+            ["created_at" => "2022-11-14 00:00:00"],
+            ["created_at" => "2022-11-14 00:00:00"],
+            ["created_at" => "2022-11-14 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-16 00:00:00"],
+            ["created_at" => "2022-11-18 00:00:00"],
+        ];
+
+        $builder = new HeatmapBuilder(
+            now: Carbon::parse("2022-11-16"),
+            decorator: new TailwindDecorator("green"),
+            timezone: new CarbonTimeZone("1"),
+        );
+
+        $result = $builder->build($data);
 
         $this->assertSame(
-            expected: [0, 0, 0, 0, 0, 0, 0, 1, 0],
+            expected: [0, 0, 0, 0, 0, 0, 1, 0],
             actual: array_map(fn(Tile $item): int => $item->isToday ? 1 : 0, $result),
         );
 
         $this->assertSame(
-            expected: [0, 0, 0, 0, 0, 0, 0, 0, 1],
+            expected: [0, 0, 0, 0, 0, 0, 0, 1],
             actual: array_map(fn(Tile $item): int => $item->inFuture ? 1 : 0, $result),
         );
     }
